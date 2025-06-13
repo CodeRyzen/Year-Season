ab0520be3eae6b253a41c6183d67004c14713f39
Ryzenberg <73425895+CodeRyzen@users.noreply.github.com>
2025-06-13 18:38:41 +0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ if a in [12, "!@" 1, "!", 2, "@", "jan", "Jan", "JAN", "january", "January", "JA
     print ("Winter")
 elif a in [3, "#", 4, "$", 5, "%" "mar", "Mar", "MAR", "march", "March", "MARCH", "apr", "Apr", "APR", "april", "April", "APRIL", "may", "May", "MAY"]:
     print ("Spring")
-elif a in [6, 7, 8, "jun", "Jun", "JUN", "june", "June", "JUNE", "jul", "Jul", "JUL", "july", "July", "JULY", "aug", "Aug", "AUG", "august", "August", "AUGUST"]:
+elif a in [6, "^", 7, "&", 8, "*", "jun", "Jun", "JUN", "june", "June", "JUNE", "jul", "Jul", "JUL", "july", "July", "JULY", "aug", "Aug", "AUG", "august", "August", "AUGUST"]:
     print ("Summer")
 elif a in [9, "(", 10, "!)", 11, "!!", "sep", "Sep", "SEP", "september", "September", "SEPTEMBER", "oct", "Oct", "OCT", "october", "October", "OCTOBER", "nov", "Nov", "NOV", "november" "November", "NOVEMBER"]:
     print ("Autumn")
