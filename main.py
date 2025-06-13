1938181be8cfb6f67ca3214adaef24ebbb852bfb
Ryzenberg <73425895+CodeRyzen@users.noreply.github.com>
2025-06-13 18:38:05 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
 a = input("Choose a month: ")
 
-if a in [1, 2, 12, "jan", "Jan", "january", "January", 
-         "feb", "Feb", "february", "February", "dec", 
-         "Dec", "december", "December"]:
+if a in [12, "!@" 1, "!", 2, "@", "jan", "Jan", "JAN", "january", "January", "JANUARY", "feb", "Feb", "FEB", "february", "February", "FEBRUARY", "dec", "Dec", "DEC", "december", "December", "DECEMBER"]:
     print ("Winter")
-elif a in [3, 4, 5, "mar", "Mar", "march", "March", "apr", 
-           "Apr", "april", "April",
-           "may", "May"]:
+elif a in [3, "#", 4, "$", 5, "%" "mar", "Mar", "MAR", "march", "March", "MARCH", "apr", "Apr", "APR", "april", "April", "APRIL", "may", "May", "MAY"]:
     print ("Spring")
-elif a in [6, 7, 8, "jun", "Jun", "jun", "June", "jul", 
-           "Jul", "july", "July", "aug", "Aug", 
-           "august", "August"]:
+elif a in [6, 7, 8, "jun", "Jun", "JUN", "june", "June", "JUNE", "jul", "Jul", "JUL", "july", "July", "JULY", "aug", "Aug", "AUG", "august", "August", "AUGUST"]:
     print ("Summer")
-elif a in [9, 10, 11, "sep", "Sep", "september", "September",
-           "oct", "Oct", "october" "October", "nov", 
-           "Nov", "november" "November"]:
+elif a in [9, "(", 10, "!)", 11, "!!", "sep", "Sep", "SEP", "september", "September", "SEPTEMBER", "oct", "Oct", "OCT", "october", "October", "OCTOBER", "nov", "Nov", "NOV", "november" "November", "NOVEMBER"]:
     print ("Autumn")
 else:
     print ("Error")
